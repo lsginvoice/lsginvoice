e48f5a618aa473da9eafc5eda8f16df6db13de1c
Brady <bkoenig122@gmail.com>
2023-08-25 10:29:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -183,5 +183,5 @@ def generate_invoice_pdf(products, subtotal, tax_amount, total_with_tax, tax_rat
     buffer.seek(0)
     return buffer.read()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)  # Use a different port, e.g., 8000
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=8000)  # Use a different port, e.g., 8000
